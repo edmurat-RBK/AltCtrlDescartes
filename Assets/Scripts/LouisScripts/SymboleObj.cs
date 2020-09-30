0321e8c5ddd6a5a0bf03c0ee8c6a45ecac7092f0
Louis <l.lefebvre@rubika-edu.com>
2020-09-30 16:03:40 +0200

    script symbole créé

---
+++ Assets/Scripts/LouisScripts/SymboleObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymboleObj : MonoBehaviour
+{
+
+    public float order;
+    public float symbol;
+    public bool validated = false;
+
+
+    void Update()
+    {
+        
+    }
+}
