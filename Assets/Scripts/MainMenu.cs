c9e1c9bd320992e665eb008de35cfa73a1daa619
Tim <t.huerre@rubika-edu.com>
2020-10-07 16:48:22 +0200

    Enabled Arrow input in Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject PlayButton;
+    public GameObject BackButton;
 public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,5 +15,13 @@ public void QuitGame ()
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+    public void UpdateEvent()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(BackButton);
+    }
+    public void ReverseEvent()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(PlayButton);
     }
 }
