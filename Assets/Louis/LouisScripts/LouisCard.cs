f1e6abf724f7e02b2fca88bb1d80f5a6205b262e
Louis <l.lefebvre@rubika-edu.com>
2020-10-08 16:46:36 +0200

    C'est réparé et y'a les graphs

--- Assets/Louis/LouisScripts/LouisCard.cs
+++ Assets/Louis/LouisScripts/LouisCard.cs
@@ -5,12 +5,42 @@ using UnityEngine;
 
 public class LouisCard : MonoBehaviour
 {
-    public Color symbol;
+    public enum Symbol
+    {
+        Null,
+        Orange,
+        Pink,
+        Blue
+    }
+    public Symbol symbole;
     public bool isHidden = true;
     public bool isValidated = false;
+    public SpriteRenderer spriteRenderer;
+    public Sprite visible;
+    public Sprite hidden;
+    public Sprite validated;
+    public bool unhidden = false;
+    public bool alreadyValidated = false;
 
     void Update()
     {
-
+        SpriteVisible();
+        SpriteValidated();
+    }
+    void SpriteVisible()
+    {
+        if (isHidden == false && unhidden == false)
+        {
+            spriteRenderer.sprite = visible;
+            unhidden = true;
+        }
+    }
+    void SpriteValidated()
+    {
+        if (isValidated == true && alreadyValidated == false)
+        {
+            spriteRenderer.sprite = validated;
+            alreadyValidated = true;
+        }
     }
 }
